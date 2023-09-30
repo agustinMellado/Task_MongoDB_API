80bf8bcc4b67f16fa2d935c615179bbf46f81d09
Agustin Mellado <aguxmellado@gmail.com>
2023-09-30 15:25:35 -0300

    Creacion de rutas post, fuciones para auth y declaraciones

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import express from "express";
 import morgan from 'morgan';
+import authRoutes from './routes/auth.routes.js';
 
 const app = express();
 
 app.use(morgan('dev'))//config para mostra msj por consola.
+
+app.use(authRoutes);
+
 export default app;
