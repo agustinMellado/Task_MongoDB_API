542ee24e57e8171b53cdf171246e2450e6303c4e
Agustin Mellado <aguxmellado@gmail.com>
2023-09-30 16:16:37 -0300

    peticion y funcion para crear el usuario

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import authRoutes from './routes/auth.routes.js';
 const app = express();
 
 app.use(morgan('dev'))//config para mostra msj por consola.
-
+app.use(express.json());// para que convertir los req.body en un formato json
 app.use('/api',authRoutes);
 
 export default app;
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,22 @@
+import Usuario from "../models/user.model.js"
 
-export const register = (req,res) => {res.send('register')}
-export const login = (req,res) => {res.send('login')}
+
+export const register = async (req, res) => {
+    const { nombre, email, password } = req.body
+    try {
+        //creo un nuevo objeto
+        const nuevoUsuario = new Usuario({
+            nombre,
+            email,
+            password
+        })
+        await nuevoUsuario.save()//guardo el nuevo usuario
+
+    } catch (error) {
+        console.log(error)
+    }
+    res.send("registrando")
+}
+export const login = (req, res) => { res.send('login') }
 
 
