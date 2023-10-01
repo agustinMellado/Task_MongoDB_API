92918c16d69245090a8dc0b9f3168ff390797eaa
Agustin Mellado <aguxmellado@gmail.com>
2023-10-01 09:48:09 -0300

    encriptacion de password

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,14 +1,17 @@
 import Usuario from "../models/user.model.js"
-
+import bcrypt from "bcryptjs"
 
 export const register = async (req, res) => {
     const { nombre, email, password } = req.body
+    //genera un hash
+    const passwordHash = await bcrypt.hash(password,10);
+    
     try {
         //creo un nuevo objeto
         const nuevoUsuario = new Usuario({
             nombre,
             email,
-            password
+            password:passwordHash//almaceno contrasena encriptada
         })
         await nuevoUsuario.save()//guardo el nuevo usuario
 
