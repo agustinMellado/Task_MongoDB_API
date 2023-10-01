89121d802da7b0d391d38ccd953d94aa94e136f6
Agustin Mellado <aguxmellado@gmail.com>
2023-10-01 11:13:42 -0300

    implementacion de sign de token y guardado de este en una cookie

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,28 +5,43 @@ import jwt from 'jsonwebtoken'
 export const register = async (req, res) => {
     const { nombre, email, password } = req.body
     //genera un hash
-    const passwordHash = await bcrypt.hash(password,10);
-    
+    const passwordHash = await bcrypt.hash(password, 10);
+
     try {
         //creo un nuevo objeto
         const nuevoUsuario = new Usuario({
             nombre,
             email,
-            password:passwordHash,//almaceno contrasena encriptada
+            password: passwordHash,//almaceno contrasena encriptada
         })
-        await nuevoUsuario.save()//guardo el nuevo usuario
-        res.json({
-            id: nuevoUsuario._id,
-            nombre: nuevoUsuario.nombre,
-            email: nuevoUsuario.email,
-            createdAt: nuevoUsuario.createdAt,
-            updatedAt: nuevoUsuario.updatedAt
+        const usuarioGuardado = await nuevoUsuario.save()//guardo el nuevo usuario
 
-        })
+        jwt.sign(
+            {
+                id: usuarioGuardado._id,
+            },
+            'fraseparageneraruntoken123',
+            {
+                expiresIn: '1d',//duracion 1 dia
+            },
+            (err, token) => {
+                if (err) console.log(err);
+                res.cookie('token', token);//lo almaceno en una cookie
+                res.json({
+                    message: 'usuario creado satisfactoriamente'
+                })
+            })
+        // res.json({
+        //     id: nuevoUsuario._id,
+        //     nombre: nuevoUsuario.nombre,
+        //     email: nuevoUsuario.email,
+        //     createdAt: nuevoUsuario.createdAt,
+        //     updatedAt: nuevoUsuario.updatedAt
+
+        // })
     } catch (error) {
         console.log(error)
     }
-    res.send("registrando")
 }
 export const login = (req, res) => { res.send('login') }
 
