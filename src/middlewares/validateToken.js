fa22079cf0418c56cc039a96a0c572eacbb99670
Agustin Mellado <aguxmellado@gmail.com>
2023-10-01 22:20:35 -0300

    utilizacion de cookies para lectura de token, condicional para verificar si es autorizado

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,5 +1,8 @@
 
-export const authRequired =(res,req,next) => {
-    console.log('token validado')
+export const authRequired =(req,res,next) => {
+    //tomo el token de los cookies
+    const {token} = req.cookies;
+    //valido token
+    if(!token) return res.status(401).json({message: 'token no autorizado'})
     next()
 }
