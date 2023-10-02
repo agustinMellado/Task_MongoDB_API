6fa8066adad0a210592df0117c8be6530f58873f
Agustin Mellado <aguxmellado@gmail.com>
2023-10-01 23:22:33 -0300

    se valido el token tomado de los cookies

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,8 +1,17 @@
-
+import jwt from 'jsonwebtoken'
 export const authRequired =(req,res,next) => {
     //tomo el token de los cookies
     const {token} = req.cookies;
-    //valido token
+    //Compruebo si se tomo un token y no algo vacio
     if(!token) return res.status(401).json({message: 'token no autorizado'})
-    next()
+   
+    //verifico que el token extraido sea valido
+    jwt.verify(token,'fraseparageneraruntoken123', (err,user) => { // codificar en un .env el token_secreto xd 
+        if(err) return res.status(403).json({message: "token invalido"})
+        //Tomo toda la informacion del usuario que estamos decodificando.
+        req.user= user;
+        next()
+    })
+
+   
 }
