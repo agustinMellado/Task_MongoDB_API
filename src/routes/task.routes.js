b7effab5af16cd208a19bcde9ac1a29725c12826
agustinMellado <aguxmellado@gmail.com>
2023-10-02 10:45:49 -0300

    crecaion de rutas con protecciones, y modificaicones en tipeado

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,19 +1,19 @@
 import { Router } from "express";
 import {authRequired} from "../middlewares/validateToken.js"
-
+import {getTasks, getTask,createTask,deleteTask,updateTask } from "../controllers/tasks.controller.js";
 const router = Router()
 /* 
           RUTAS CRUD 
 */
 //Tomar todos los elementos
-router.get('/task', authRequired,)
+router.get('/task', authRequired,getTasks)
 //toma un solo elemento
-router.get('/task:id', authRequired,)
+router.get('/task:id', authRequired,getTask)
 //crear un elemento
-router.post('/task', authRequired,)
+router.post('/task', authRequired,createTask)
 //borra un elemento
-router.delete('/task:id', authRequired,)
+router.delete('/task:id', authRequired,deleteTask)
 //actualizar un elemento
-router.put('/task:id', authRequired,)
+router.put('/task:id', authRequired,updateTask)
 
 export default router
