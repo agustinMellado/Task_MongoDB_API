28a4291099344d6ceacf0b1aebcb0f5bba81e6f4
agustinMellado <aguxmellado@gmail.com>
2023-10-02 10:37:51 -0300

    creacion de estructura de controladores

---
+++ src/controllers/tasks.controller.js
@@ -0,0 +1,8 @@
+
+
+export const getTasks= async(req, res)=>{};
+export const createTasks= async(req, res)=>{};
+export const getTask= async(req, res)=>{};
+export const updateTasks= async(req, res)=>{};
+export const deleteTasks= async(req, res)=>{};
+
