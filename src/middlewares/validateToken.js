459ed58783ce132d7126ecdcd1479e95a7df3992
Agustin Mellado <aguxmellado@gmail.com>
2023-10-01 21:52:27 -0300

    esqueleto para validar token

---
+++ src/middlewares/validateToken.js
@@ -0,0 +1,4 @@
+
+export const authRequired =(res,req,next) => {
+    console.log('token validado')
+}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { register ,login, cerrarSesion,profile} from "../controllers/auth.controller.js";
-
+import { register, login, cerrarSesion, profile } from "../controllers/auth.controller.js";
+import { authRequired } from "../middlewares/validateToken.js";
 const router = Router()
 
-router.post('/register',register)
-router.post('/login',login)
-router.post('/cerrarSesion',cerrarSesion)
-router.get('/profile',profile)
+router.post('/register', register)
+router.post('/login', login)
+router.post('/cerrarSesion', cerrarSesion)
+router.get('/profile',authRequired, profile)
 
 export default router;
