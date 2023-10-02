0f66546632921124e9b33b52a7edb1df6bdc652e
agustinMellado <aguxmellado@gmail.com>
2023-10-02 10:24:03 -0300

    se generaron las rutas para el crud y se declararon en el archivo principal

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from 'morgan';
 import authRoutes from './routes/auth.routes.js';
+import taskRoutes from './routes/task.routes.js'
 import cookieParser from 'cookie-parser'
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(morgan('dev'))//config para mostra msj por consola.
 app.use(express.json());// para que convertir los req.body en un formato json
 app.use(cookieParser());
 app.use('/api',authRoutes);
+app.use('/api', taskRoutes)
 
 export default app;
---
+++ src/routes/task.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import {authRequired} from "../middlewares/validateToken.js"
+
+const router = Router()
+/* 
+          RUTAS CRUD 
+*/
+//Tomar todos los elementos
+router.get('/task', authRequired,)
+//toma un solo elemento
+router.get('/task', authRequired,)
+//crear un elemento
+router.post('/task', authRequired,)
+//borra un elemento
+router.delete('/task:id', authRequired,)
+//actualizar un elemento
+router.put('/task:id', authRequired,)
+
+export default router
