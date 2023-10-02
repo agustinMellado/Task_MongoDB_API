e0e62752d9ead2857b95f67291e61f500d468bdb
agustinMellado <aguxmellado@gmail.com>
2023-10-02 11:09:32 -0300

    funcion para ver todas las tareas y para crear una nueva tarea

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,7 +1,20 @@
+import Task from "../models/task.model";
 
-
-export const getTasks= async(req, res)=>{};
-export const createTask= async(req, res)=>{};
+export const getTasks= async(req, res)=>{
+    const tasks = await Task.find()
+};
+export const createTask= async(req, res)=>{
+    //tomo los valores del front
+    const {title,description,date}=req.body;
+    //Creo el objeto nuevo 
+    const newTask= new Task({
+        title,
+        description,
+        date
+    })
+    //la guardo en la bd
+    await newTask.save()
+};
 export const getTask= async(req, res)=>{};
 export const updateTask= async(req, res)=>{};
 export const deleteTask= async(req, res)=>{};
