96fa4cef8a5265984adba24ea0ae7f6e8487b4e4
Agustin Mellado <aguxmellado@gmail.com>
2023-10-09 17:25:37 -0300

    modificacion de rutas para tomar id

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -8,12 +8,12 @@ const router = Router()
 //Tomar todos los elementos
 router.get('/task', authRequired,getTasks)
 //toma un solo elemento
-router.get('/task:id', authRequired,getTask)
+router.get('/task/:id', authRequired,getTask)
 //crear un elemento
 router.post('/task', authRequired,createTask)
 //borra un elemento
-router.delete('/task:id', authRequired,deleteTask)
+router.delete('/task/:id', authRequired,deleteTask)
 //actualizar un elemento
-router.put('/task:id', authRequired,updateTask)
+router.put('/task/:id', authRequired,updateTask)
 
 export default router
