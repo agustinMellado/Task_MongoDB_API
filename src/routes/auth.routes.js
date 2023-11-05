48403f72dc4f284f0c6866e565cbbbf6d37bc36e
Agustin Mellado <aguxmellado@gmail.com>
2023-11-05 16:26:11 -0300

    se aplicacion los middlewares de valdicacion de datos de usuario en las rutas

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,14 +2,14 @@ import { Router } from "express";
 import { register, login, cerrarSesion, profile } from "../controllers/auth.controller.js";
 import { authRequired } from "../middlewares/validateToken.js";
 import { validateSchema } from "../middlewares/validator.middleware.js";
-import {registerSchema, loginSchema } from "../schemas/auth.schema.js"
+import { registerSchema, loginSchema } from "../schemas/auth.schema.js"
 
 
 const router = Router()
 
-router.post('/register',validateSchema(registerSchema), register)
-router.post('/login',validateSchema(loginSchema), login)
+router.post('/register', validateSchema(registerSchema), register)
+router.post('/login', validateSchema(loginSchema), login)
 router.post('/cerrarSesion', cerrarSesion)
-router.get('/profile',authRequired, profile)
+router.get('/profile', authRequired, profile)
 
 export default router;
