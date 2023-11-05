89ad23a7def78159d690dd8a9009a48ecfc45d72
Agustin Mellado <aguxmellado@gmail.com>
2023-11-05 16:21:46 -0300

    se genero la funcion para validar los schemas

---
+++ src/middlewares/validator.middleware.js
@@ -0,0 +1,10 @@
+export const validateSchema =(schema) => (req,res,next)=>{
+    try{// valida los datos
+        schema.parse(req.body);
+        //si los valida correctqamente continuan
+        next()
+    }catch(error){//si falla
+        return res.status(400).json({error})
+    }
+    
+}
