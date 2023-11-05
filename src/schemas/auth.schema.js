f7b1b97b43a133e8045fac967337e1daff319957
Agustin Mellado <aguxmellado@gmail.com>
2023-11-05 16:14:10 -0300

    se genero una funcion par validar el login

--- src/schemas/auth.schema.js
+++ src/schemas/auth.schema.js
@@ -1,17 +1,30 @@
-import {z} from 'zod'
+import { z } from 'zod'
 
-export const registerSchema= z.object({
-username: z.string({
-    required_error: 'El nombre de usuario es requerido.'
-}),
-email: z.string({
-    required_error: 'El email es requerido.'
-}).email({
-    message:'Email invalido'
-}),
-password: z.string({
-    required_error: 'La contraseña es requerida.'
-}).min(6,{
-    message:'La contraseña debe tener al menos 6 caracteres.'
-}),
+export const registerSchema = z.object({
+    username: z.string({
+        required_error: 'El nombre de usuario es requerido.'
+    }),
+    email: z.string({
+        required_error: 'El email es requerido.'
+    }).email({
+        message: 'Email invalido'
+    }),
+    password: z.string({
+        required_error: 'La contraseña es requerida.'
+    }).min(6, {
+        message: 'La contraseña debe tener al menos 6 caracteres.'
+    }),
+})
+
+export const loginUser = z.object({
+    email: z.string({
+        required_error: 'El email es requerido.'
+    }).email({
+        message: 'Email invalido'
+    }),
+    password: z.string({
+        required_error: 'La contraseña es requerida.'
+    }).min(6, {
+        message: 'La contraseña debe tener al menos 6 caracteres.'
+    }),
 })
