4b2b9ac68cb9aedcff68caa99eafdaa675702bff
Agustin Mellado <aguxmellado@gmail.com>
2023-11-05 16:52:20 -0300

    se aplico middleware para validar la creacion de una tarea a las rutas de tareas

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -2,6 +2,9 @@ import { Router } from "express";
 import {authRequired} from "../middlewares/validateToken.js"
 import {getTasks, getTask,createTask,deleteTask,updateTask } from "../controllers/tasks.controller.js";
 import cacheInit from "../middlewares/cache.js";
+import { validateSchema } from "../middlewares/validator.middleware.js";
+import {  createTaskSchema} from "../schemas/auth.schema.js"
+
 const router = Router()
 /* 
           RUTAS CRUD 
@@ -11,7 +14,7 @@ router.get('/task', authRequired,cacheInit,getTasks)
 //toma un solo elemento
 router.get('/task/:id', authRequired,getTask)
 //crear un elemento
-router.post('/task', authRequired,createTask)
+router.post('/task', authRequired,validateSchema(createTaskSchema),createTask)
 //borra un elemento
 router.delete('/task/:id', authRequired,deleteTask)
 //actualizar un elemento
