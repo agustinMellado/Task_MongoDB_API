4b5c288d0bf6dd4ecab7aaeee4c9d7792728115c
Agustin Mellado <aguxmellado@gmail.com>
2023-11-05 16:52:28 -0300

    se aplico middleware para validar la creacion de una tarea a las rutas de tareas

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,23 +1,23 @@
 import { Router } from "express";
-import {authRequired} from "../middlewares/validateToken.js"
-import {getTasks, getTask,createTask,deleteTask,updateTask } from "../controllers/tasks.controller.js";
+import { authRequired } from "../middlewares/validateToken.js"
+import { getTasks, getTask, createTask, deleteTask, updateTask } from "../controllers/tasks.controller.js";
 import cacheInit from "../middlewares/cache.js";
 import { validateSchema } from "../middlewares/validator.middleware.js";
-import {  createTaskSchema} from "../schemas/auth.schema.js"
+import { createTaskSchema } from "../schemas/auth.schema.js"
 
 const router = Router()
 /* 
           RUTAS CRUD 
 */
 //Tomar todos los elementos
-router.get('/task', authRequired,cacheInit,getTasks)
+router.get('/task', authRequired, cacheInit, getTasks)
 //toma un solo elemento
-router.get('/task/:id', authRequired,getTask)
+router.get('/task/:id', authRequired, getTask)
 //crear un elemento
-router.post('/task', authRequired,validateSchema(createTaskSchema),createTask)
+router.post('/task', authRequired, validateSchema(createTaskSchema), createTask)
 //borra un elemento
-router.delete('/task/:id', authRequired,deleteTask)
+router.delete('/task/:id', authRequired, deleteTask)
 //actualizar un elemento
-router.put('/task/:id', authRequired,updateTask)
+router.put('/task/:id', authRequired, updateTask)
 
 export default router
