b4d614db6fc7e65b7fc892a6d6e442ada741268b
Agustin Mellado <aguxmellado@gmail.com>
2023-11-16 09:31:48 -0300

    se agregaron estilos

--- client/src/pages/auth/RegisterPage.jsx
+++ client/src/pages/auth/RegisterPage.jsx
@@ -2,7 +2,7 @@ import { useForm } from "react-hook-form";
 function RegisterPage() {
   const { register, handleSubmit } = useForm();
   return (
-    <div class="bg-zinc-800 max-w-md p-10 rounded-md">
+    <div className="bg-zinc-800 max-w-md p-10 rounded-md">
       {/* formulario */}
       <form
         onSubmit={handleSubmit((values) => {
@@ -10,9 +10,14 @@ function RegisterPage() {
         })}
       >
         <input type="text" {...register("username", { required: true })} 
-          class="w-full bg-zinc-700 text-white px-4 py-2 rounded-md"/>
-        <input type="text" {...register("email", { required: true })} />
-        <input type="text" {...register("password", { required: true })} />
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md m-2"
+          placeholder="Ingrese su nombre"/>
+        <input type="text" {...register("email", { required: true })} 
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md m-2"
+          placeholder="Ingrese su email"/>
+        <input type="text" {...register("password", { required: true })} 
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md m-2"
+          placeholder="Ingrese su contraseña"/>
         <button type="submit">Registrar</button>
       </form>
     </div>
