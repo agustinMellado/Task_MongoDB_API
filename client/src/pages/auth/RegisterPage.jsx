725dc76291e3a0f60b0cc4a67bfe5cf6af5ab9f3
Agustin Mellado <aguxmellado@gmail.com>
2023-11-16 09:05:07 -0300

    configuracion de tailwind

--- client/src/pages/auth/RegisterPage.jsx
+++ client/src/pages/auth/RegisterPage.jsx
@@ -1,21 +1,22 @@
-
+import { useForm } from "react-hook-form";
 function RegisterPage() {
+  const { register, handleSubmit } = useForm();
   return (
-    <div>
+    <div class="bg-zinc-800 max-w-md p-10 rounded-md">
       {/* formulario */}
       <form
-        onSubmit={handleSubmit((values)=>{
+        onSubmit={handleSubmit((values) => {
           console.log(values);
-        })}>
-        <input type="text" {...register("username",{ required: true})} />
-        <input type="text" {...register("email",{ required: true})} />
-        <input type="text" {...register("password",{ required: true})} />
+        })}
+      >
+        <input type="text" {...register("username", { required: true })} 
+          class="w-full bg-zinc-700 text-white px-4 py-2 rounded-md"/>
+        <input type="text" {...register("email", { required: true })} />
+        <input type="text" {...register("password", { required: true })} />
         <button type="submit">Registrar</button>
       </form>
-      
-      
     </div>
-  )
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
