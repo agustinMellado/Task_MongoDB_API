2d81fd9e0fa9d9c429f1c3787f8f62e62ff95714
Agustin Mellado <aguxmellado@gmail.com>
2023-11-18 19:58:25 -0300

    creacion de componentes pages y configuracion de rutas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegisterPage from "./pages/auth/RegisterPage";
 import LoginPage from "./pages/auth/LoginPage";
 import { AuthProvider } from "./context/authContext";
+import TaskPage from "./pages/task/TaskPage";
+import TaskFormPage from "./pages/task/TaskFormPage";
+import ProfilePage from "./pages/profile/ProfilePage";
 
 function App() {
   return (
@@ -13,10 +16,10 @@ function App() {
         <Route path="/" element={<h1>Home page</h1>}/>
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/register" element={<RegisterPage/>}/>
-        <Route path="/tasks" element={<h1>Task page</h1>}/>
-        <Route path="/add-task" element={<h1>new Task</h1>}/>
-        <Route path="/tasks/:id" element={<h1>update Task</h1>}/>
-        <Route path="/profile" element={<h1>Profile page</h1>}/>
+        <Route path="/tasks" element={<TaskPage/>}/>
+        <Route path="/add-task" element={<TaskFormPage/>}/>
+        <Route path="/tasks/:id" element={<TaskFormPage/>}/>
+        <Route path="/profile" element={<h1><ProfilePage/></h1>}/>
       </Routes>
     </BrowserRouter>
     </AuthProvider>
