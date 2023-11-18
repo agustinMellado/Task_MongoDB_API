f994506839f0c76752c6f75fe84193e8950f88ee
Agustin Mellado <aguxmellado@gmail.com>
2023-11-18 11:38:57 -0300

    creacion de context y peticiones

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 const API = 'http://localhost:3000/api'
-// Creo un registerRequest que le pasamos un usuario 
+// Creo peticiones que le pasamos un usuario 
 export const registerRequest = (user)=> axios.post(`${API}/register`, user)//enviamos un post a la direccion con el user asignado
 export const loginRequest = (user)=> axios.post(`${API}/login`,user)//enviamos un post a la direccion con el user asignado
