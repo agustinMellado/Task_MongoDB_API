80235db3d23bbb03215c77c4ebb951819232dd1c
Agustin Mellado <aguxmellado@gmail.com>
2023-11-20 17:12:05 -0300

    estilo global

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import TaskFormPage from "./pages/task/TaskFormPage";
 import ProfilePage from "./pages/profile/ProfilePage";
 import HomePage from "./pages/home/HomePage";
 import ProtectedRoutes from "./ProtectedRoutes";
-import{TaskProvider} from "./context/taskContext";
+import { TaskProvider } from "./context/taskContext";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -16,19 +16,21 @@ function App() {
     <AuthProvider>
       <TaskProvider>
         <BrowserRouter>
-        <Navbar/>
-          <Routes>
-            //Rutas
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route element={<ProtectedRoutes />}>
-              <Route path="/tasks" element={<TaskPage />} />
-              <Route path="/add-task" element={<TaskFormPage />} />
-              <Route path="/tasks/:id" element={<TaskFormPage />} />
-              <Route path="/profile" element={<ProfilePage />} />
-            </Route>
-          </Routes>
+        <Navbar />
+          <main className="container mx-auto px-10">
+            <Routes>
+              //Rutas
+              <Route path="/" element={<HomePage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+              <Route element={<ProtectedRoutes />}>
+                <Route path="/tasks" element={<TaskPage />} />
+                <Route path="/add-task" element={<TaskFormPage />} />
+                <Route path="/tasks/:id" element={<TaskFormPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+              </Route>
+            </Routes>
+          </main>
         </BrowserRouter>
       </TaskProvider>
     </AuthProvider>
