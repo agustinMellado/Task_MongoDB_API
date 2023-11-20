c8cc7dcd7af95a49a910e7a602590b4052d486ed
Agustin Mellado <aguxmellado@gmail.com>
2023-11-20 19:05:21 -0300

    funcionalidad al boton borrar con funciones y refresh a la lista de tareas

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,10 +1,20 @@
+import { useTasks } from "../context/taskContext";
+
+
+
+
 function TaskCard({ task }) {
+  const{deleteTask}=useTasks()
   return (
     <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
       <header className="flex justify-between">
         <h1 className="text-2xl font-bold">{task.title}</h1>
         <div className="flex gap-x-2 items-center">
-          <button>Borrar</button>
+          <button
+          onClick={() =>{
+            deleteTask(task._id);
+          }}
+          >Borrar</button>
           <button>Editar</button>
         </div>
       </header>
--- client/src/context/TaskContext.jsx
+++ client/src/context/TaskContext.jsx
@@ -1,5 +1,9 @@
 import { createContext, useContext, useState } from "react";
-import { createTaskRequest, getTasksRequest } from "../api/task";
+import {
+  createTaskRequest,
+  getTasksRequest,
+  deleteTaskRequest,
+} from "../api/task";
 
 const TaskContext = createContext();
 export const useTasks = () => {
@@ -15,19 +19,29 @@ export function TaskProvider({ children }) {
   const [tasks, setTasks] = useState([]);
   //obtener tareas
   const getTasks = async () => {
-   try {
-    const res = await getTasksRequest();
-    setTasks(res.data)
-   } catch (error) {
-    console.error(error);
-    
-   }
+    try {
+      const res = await getTasksRequest();
+      setTasks(res.data);
+    } catch (error) {
+      console.error(error);
+    }
   };
   //creacion de tareas
   const createTask = async (task) => {
     try {
       const res = await createTaskRequest(task);
       console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  //borrar tarea
+  const deleteTask = async (id) => {
+    try {
+
+      const res = await deleteTaskRequest(id);
+      //si se borro que refresque y llame a los existentes
+      if(res.status=== 204) setTasks(tasks.filter(task=> task._id != id))
     } catch (error) {
       console.log(error);
     }
@@ -39,6 +53,7 @@ export function TaskProvider({ children }) {
         tasks,
         createTask,
         getTasks,
+        deleteTask,
       }}
     >
       {children}
